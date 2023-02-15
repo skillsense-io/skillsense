87c43fefbe6a5612bd9bc5e90a84c900b4b5f237
James Cavallo <jamescavallo@Jamess-MBP.lan>
2023-02-14 19:45:52 -0500

    SSL updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "landing#index"
+
+  if Rails.env.production?
+    constraints protocol: "http://" do
+      match "(*any)" => redirect { |params, request|
+        URI.parse(request.url).tap { |uri| uri.scheme = "https" }.to_s
+      }, via: [:get, :post]
+    end
+  end
+  
 end
